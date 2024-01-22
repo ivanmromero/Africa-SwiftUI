bd7c4375b354a8cb741ea71c7afdb1c215af2150
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-01-22 20:29:59 -0300

    Initial config

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Ivan Romero on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
